9092a14df90cc96342cb209bd17e556ec375158f
Cameron Little <cameron@camlittle.com>
2016-02-13 13:41:35 -0800

    Added PUBLISH_URI check

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 #
 
+if [ -z "$PUBLISH_URI" ]; then
+    echo "PUBLISH_URI env must be set"
+    exit 1
+fi
+
 VERSION=$(jq -r < "${GITDIR}./package.json" .version)
 rsync --no-R --no-implied-dirs cmd-shift-t.safariextz "${PUBLISH_URI}./cmd-shift-t_${VERSION}.safariextz"
